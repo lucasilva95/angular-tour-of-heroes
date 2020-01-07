8ff37434024c0666244893acc7d7a9051445637e
lucasilva95 <lucascsilva95@gmail.com>
2020-01-07 15:51:32 -0300

    6

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Chaves' },
+  { id: 12, name: 'Chiquinha' },
+  { id: 13, name: 'Sr.º Madruga' },
+  { id: 14, name: 'D.ª Clotilde' },
+  { id: 15, name: 'D.ª Florinda' },
+  { id: 16, name: 'Prof.º Girafales' },
+  { id: 17, name: 'Kiko' },
+  { id: 18, name: 'Sr.º Barriga' },
+  { id: 19, name: 'Nhonho' },
+  { id: 20, name: 'Pópis' }
 ];
